da90e9fe30afe4349edd6f75294fe3b4627754f3
Nicolas Coulon <NICOLAS.X.COULON@GSK.COM>
2021-11-08 20:53:52 +0100

    fix memory leak happening when resubscribing

--- examples/pubsub-basic.js
+++ examples/pubsub-basic.js
@@ -5,8 +5,12 @@ createBroker({ port: 8888 })
 const client1 = createPubSubClient({ host: '::', port: 8888 })
 const client2 = createPubSubClient({ host: '::', port: 8888 })
 
-client1.publish('some-topic', { hello: 'world' })
+client1.publish('some-topic', { hello: 'world1' })
 
 client2.subscribe('some-topic', (message) => {
   console.log(message)
 })
+
+setTimeout(() => {
+  client1.publish('some-topic', { hello: 'world2' })
+}, 5000)
