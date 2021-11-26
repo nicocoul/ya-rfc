fc7196dc2b24b0990e6732dbd7f5963a17d8c516
Nicolas Coulon <NICOLAS.X.COULON@GSK.COM>
2021-11-26 11:49:43 +0100

    Proxy client.execute

--- examples/rpc-ws.js
+++ examples/rpc-ws.js
@@ -8,13 +8,21 @@ broker.plug(ya.plugins.ws(new WebSocketServer({ port: 8004 })))
 const modulePath = path.join(__dirname, 'procedures.js')
 ya.server.ws({ host: 'localhost', port: 8004 }, modulePath)
 
-// execute procedure count from the client
 const rpcClient = ya.client.ws({ host: 'localhost', port: 8004 })
-rpcClient.execute('count', [10000], (err, data) => {
-  if (!err) {
-    console.log(data)
-  }
-})
+// execute procedure count from the client
+// rpcClient.execute('count', [10000], (err, data) => {
+//   if (!err) {
+//     console.log(data)
+//   }
+// })
+
+rpcClient.remote.count(100, { onProgress: console.log, onStatus: console.log })
+  .then(result => {
+    console.log(result)
+  })
+  .catch(error => {
+    console.error(error)
+  })
 
 /* output:
 10000
